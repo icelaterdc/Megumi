00bec4420a0c00e4e42bd9c8116ae2c77288cee5
nuekkis <120577848+icelaterdc@users.noreply.github.com>
2025-10-01 19:35:34 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,25 @@
+// settings.gradle.kts (repo köküne koyup mevcut dosyanın yerine yaz)
+pluginManagement {
+    repositories {
+        // pluginleri buradan çözümle (Android plugin Google Maven'da olduğu için google() burada önemli)
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+    plugins {
+        // (opsiyonel) burada plugin versiyonlarını da sabitleyebilirsin
+        id("com.android.application") version "8.1.1" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "KizzyLikeApp"
 include(":app")
